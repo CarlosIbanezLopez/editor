39d62ce2c8bc2acf7839732f7b2cf46282b0e2db
CarlosIbanezLopez <116035301+CarlosIbanezLopez@users.noreply.github.com>
2025-04-29 09:05:33 -0400

    projexc

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     // 🔥 PROYECTOS PÚBLICOS para usuarios autenticados
